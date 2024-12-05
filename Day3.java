3154b1e0848cab0d7888850e8262841536081fdc
MichaelKarabinosh <michaelk368@nycstudents.net>
2024-12-05 10:48:02 -0500

    Update Day3.java

--- Day3.java
+++ Day3.java
@@ -1,8 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,26 +11,68 @@ public class Day3 {
 
         ArrayList<String> fileData = getFileData("src/InputFile");
         // you now have a list of Strings from the file "InputFile"
+        ArrayList<String> SimpleMatches = new ArrayList<String>();
+        ArrayList<String> AdvMatches = new ArrayList<String>();
 
-
-boolean go = true;
-int sum = 0;
+        boolean go = true;
+        int sum = 0;
         String q = fileData.getFirst();
-        Pattern instruction = Pattern.compile("mul\\(\\d+,\\d+\\)");
-        Matcher matcher = instruction.matcher(q);
-        while (matcher.find())
+        Pattern advinstruction = Pattern.compile("mul\\(\\d+,\\d+\\)|don't\\(\\)|do\\(\\)");
+        Pattern simpleinstruction = Pattern.compile("mul\\(\\d+,\\d+\\)");
+//        Pattern good = Pattern.compile("do\\(\\)");
+//        Pattern bad = Pattern.compile("don't\\(\\)");
+        Matcher advmatcher = advinstruction.matcher(q);
+        Matcher simplematcher = simpleinstruction.matcher(q);
+        while (simplematcher.find()) {
+            SimpleMatches.add(simplematcher.group());
+        }
+        while (advmatcher.find())
         {
-                String s = matcher.group().substring(4);
+            AdvMatches.add(advmatcher.group());
+        }
+        System.out.println(SimpleMatches);
+        for (int i = 0; i < SimpleMatches.size(); i++)
+        {
+            String s = SimpleMatches.get(i).substring(4);
+            s = s.replace("(","");
+            s = s.replace(")", "");
+            String[] a = s.split(",");
+            int x = Integer.parseInt(a[0]);
+            int y = Integer.parseInt(a[1]);
+            sum += x * y;
+        }
+        System.out.println("part one: " + sum);
+        sum = 0;
+
+        for (int i = 0; i < AdvMatches.size(); i++)
+        {
+            String s = AdvMatches.get(i);
+            if (s.equals("do()"))
+            {
+                go = true;
+            }
+            else if (s.equals("don't()"))
+            {
+                go = false;
+            }
+            else if (go && s.contains("mul("))
+            {
+                s = s.substring(4);
                 s = s.replace("(","");
                 s = s.replace(")", "");
                 String[] a = s.split(",");
                 int x = Integer.parseInt(a[0]);
                 int y = Integer.parseInt(a[1]);
                 sum += x * y;
+            }
         }
-        System.out.println("part one: " + sum);
+        System.out.println("part two: " + sum);
+        {
+
 
         }
+
+    }
 
 
 
