4dc00723a90f239261fd160590584405cd36a348
MichaelKarabinosh <michaelk368@nycstudents.net>
2024-12-12 00:24:14 -0500

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day6 {
@@ -8,100 +9,250 @@ public class Day6 {
 
         ArrayList<String> fileData = getFileData("src/InputFile");
         // you now have a list of Strings from the file "InputFile"
-        String[][] map = new String[130][130];
-        int xpos = 0;
-        int ypos = 0;
+        String [] [] a = new String[130][130];
+        for (int i = 0; i < fileData.size(); i++)
+        {
+            String line = fileData.get(i);
+            for(int j = 0; j <line.length(); j++)
+            {
+                a[i][j] = line.substring(j, j + 1);
+                if (a[i][j].equals("^"))
+                {
+                    System.out.println(i + "" + j);
+                }
+            }
+        }
+        String[][] c = deepCopy(a);
+        System.out.println(countVisits(a));;
+        int loops = 0;
+        for (int i = 0; i < 130; i++)
+        {
+            for (int j = 0; j < 130; j++)
+            {
+                String[][] b = deepCopy(c);
+                    b[i][j] = "#";
+                if (isLoop(b))
+                {
+                    loops++;
+                }
+            }
+        }
+        System.out.println(loops);
+    }
+
+
+    public static int countVisits(String[][] a)
+    {
+        int steps = 0;
+        int ypos = 75;
+        int xpos = 73;
         boolean up = true;
         boolean right = false;
         boolean down = false;
         boolean left = false;
-        String temp = "";
-        boolean roadblock = false;
-        for (int i = 0; i < fileData.size(); i++) {
-            String line = fileData.get(i);
-            for (int j = 0; j < line.length(); j++) {
-                if (line.substring(j, j + 1).equals("^")) {
-                    xpos = j;
-                    ypos = i;
-                }
-                map[i][j] = line.substring(j, j + 1);
+        boolean outOfBounds = false;
+        int visits = 0;
+        boolean loop = false;
+        a[ypos - 1][xpos] = "^";
+        while (xpos < 131)
+        {
+            if (steps > 10000)
+            {
+                loop = true;
+                break;
             }
-        }
-        while (xpos < 129 || ypos < 129) {
+            if (xpos == 130)
+            {
+                break;
+            }
+            if (ypos == 130)
+            {
+                break;
+            }
+            if (xpos == -1)
+            {
+                break;
+            }
+            if (ypos == -1)
+            {
+                break;
+            }
             try {
-                if (map[ypos][xpos].equals("#")) {
-                    roadblock = true;
-                    if (up) {
+                if (up) {
+                    if (a[ypos - 1][xpos].equals("#")) {
                         up = false;
                         right = true;
-                    } else if (right) {
+                    }
+                }
+                if (right) {
+                    if (a[ypos][xpos + 1].equals("#")) {
                         right = false;
                         down = true;
-                    } else if (down) {
+                    }
+                }
+                if (down) {
+
+                    if (a[ypos + 1][xpos].equals("#")) {
                         down = false;
                         left = true;
-                    } else {
+                    }
+                }
+                if (left) {
+                    if (a[ypos][xpos - 1].equals("#")) {
                         left = false;
                         up = true;
                     }
                 }
-            } catch (ArrayIndexOutOfBoundsException _) {
+            }
+            catch (Exception _)
+            {
 
             }
-            try {
-                if (!roadblock) {
-                    map[ypos][xpos] = "X";
+            if (up)
+            {
+                a[ypos][xpos] = "X";
+                ypos--;
+                steps++;
+            }
+            if (right)
+            {
+                a[ypos][xpos] = "X";
+                xpos++;
+                steps++;
+            }
+            if (down)
+            {
+                a[ypos][xpos] = "X";
+                ypos++;
+                steps++;
+            }
+            if (left)
+            {
+                a[ypos][xpos] = "X";
+                xpos--;
+                steps++;
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < a.length; i++)
+        {
+            for (int j = 0; j < a[i].length; j++)
+            {
+                if (a[i][j].equals("X"))
+                {
+                    count++;
                 }
-                if (roadblock) {
-                    if (right) {
-                        ypos++;
-                    }
-                    if (down) {
-                        xpos--;
-                    }
-                    if (left) {
-                        ypos--;
-                    }
-                    if (up) {
-                        xpos++;
-                    }
-                    roadblock = false;
-                }
+            }
+        }
+        return count;
+    }
 
-            } catch (ArrayIndexOutOfBoundsException _) {
 
+    public static boolean isLoop(String[][] a)
+    {
+        int steps = 0;
+        int ypos = 75;
+        int xpos = 73;
+        boolean up = true;
+        boolean right = false;
+        boolean down = false;
+        boolean left = false;
+        while (steps < 99999)
+        {
+            if (steps > 10000)
+            {
+                return true;
+            }
+            if (xpos == 130)
+            {
+                break;
+            }
+            if (ypos == 130)
+            {
+                break;
+            }
+            if (xpos == -1)
+            {
+                break;
+            }
+            if (ypos == -1)
+            {
+                break;
             }
             try {
                 if (up) {
-                    ypos--;
-                } else if (right) {
-                    xpos++;
-                } else if (down) {
-                    ypos++;
-                } else {
-                    xpos--;
+                    if (a[ypos - 1][xpos].equals("#")) {
+                        up = false;
+                        right = true;
+                    }
                 }
-            } catch (ArrayIndexOutOfBoundsException _) {
+                if (right) {
+                    if (a[ypos][xpos + 1].equals("#")) {
+                        right = false;
+                        down = true;
+                    }
+                }
+                if (down) {
 
-            }
-            try {
-                temp = map[ypos][xpos];
-            } catch (ArrayIndexOutOfBoundsException _) {
-                break;
-            }
-        }
-            int count = 0;
-            for (int i = 0; i < fileData.size(); i++) {
-                String line = fileData.get(i);
-                for (int j = 0; j < line.length(); j++) {
-                    if (map[i][j].equals("X")) {
-                        count++;
+                    if (a[ypos + 1][xpos].equals("#")) {
+                        down = false;
+                        left = true;
+                    }
+                }
+                if (left) {
+                    if (a[ypos][xpos - 1].equals("#")) {
+                        left = false;
+                        up = true;
                     }
                 }
             }
-            System.out.println("part one: " + count);
+            catch (Exception _)
+            {
 
+            }
+            if (up)
+            {
+                a[ypos][xpos] = "X";
+                ypos--;
+                steps++;
+            }
+            if (right)
+            {
+                a[ypos][xpos] = "X";
+                xpos++;
+                steps++;
+            }
+            if (down)
+            {
+                a[ypos][xpos] = "X";
+                ypos++;
+                steps++;
+            }
+            if (left)
+            {
+                a[ypos][xpos] = "X";
+                xpos--;
+                steps++;
+            }
+        }
+        return false;
     }
+
+    public static String[][] deepCopy(String[][] original) {
+        if (original == null) {
+            return null;
+        }
+
+        final String[][] result = new String[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            result[i] = Arrays.copyOf(original[i], original[i].length);
+            // For Java versions prior to Java 6 use the next:
+            // System.arraycopy(original[i], 0, result[i], 0, original[i].length);
+        }
+        return result;
+    }
+
+
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
