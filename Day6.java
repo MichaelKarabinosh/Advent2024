66a6cca515ab966e36af44c808dc5a5ee758f8f2
MichaelKarabinosh <michaelk368@nycstudents.net>
2024-12-12 22:18:13 -0500

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Day6 {
     public static void main(String[] args) {
 
+
         ArrayList<String> fileData = getFileData("src/InputFile");
         // you now have a list of Strings from the file "InputFile"
-        String [] [] a = new String[130][130];
+        int len = fileData.size();
+        String [] [] a = new String[len][len];
         int x1 = 0;
         int y1 = 0;
         for (int i = 0; i < fileData.size(); i++)
@@ -20,47 +23,50 @@ public class Day6 {
                 a[i][j] = line.substring(j, j + 1);
                 if (a[i][j].equals("^"))
                 {
-                    System.out.println(i + "" + j);
                     x1 = i;
                     y1 = j;
                 }
             }
         }
         String[][] c = deepCopy(a);
-        System.out.println(countVisits(a, x1, y1));;
+        System.out.println("part one: " +countVisits(a, x1, y1));
         int loops = 0;
-        for (int i = 0; i < 130; i++)
-        {
-            for (int j = 0; j < 130; j++)
-            {
-                String[][] b = deepCopy(c);
-                b[i][j] = "#";
-                if (isLoop(b, x1, y1))
-                {
-                    loops++;
-                }
+        for (String visit : visits) {
+            String[][] b = deepCopy(c);
+            String[] pop = visit.split(" ");
+            b[Integer.parseInt(pop[0])][Integer.parseInt(pop[1])] = "#";
+            if (isLoop(b, x1, y1)) {
+                loops++;
             }
+
+
         }
-        System.out.println(loops);
+        System.out.println("part two: " + loops);
     }
+
+
+    public static ArrayList<String> visits = new ArrayList<>();
+
+
 
 
     public static int countVisits(String[][] a, int x, int y)
     {
+        int count = 0;
+        int len = a.length;
         int ypos = x;
         int xpos = y;
         boolean up = true;
         boolean right = false;
         boolean down = false;
         boolean left = false;
-        a[ypos - 1][xpos] = "^";
-        while (xpos < 131)
+        while (xpos < len + 1)
         {
-            if (xpos == 130)
+            if (xpos == len)
             {
                 break;
             }
-            if (ypos == 130)
+            if (ypos == len)
             {
                 break;
             }
@@ -86,7 +92,6 @@ public class Day6 {
                     }
                 }
                 if (down) {
-
                     if (a[ypos + 1][xpos].equals("#")) {
                         down = false;
                         left = true;
@@ -101,36 +106,32 @@ public class Day6 {
             }
             catch (Exception _)
             {
-
             }
+            a[ypos][xpos] = "X";
             if (up)
             {
-                a[ypos][xpos] = "X";
                 ypos--;
             }
             if (right)
             {
-                a[ypos][xpos] = "X";
                 xpos++;
             }
             if (down)
             {
-                a[ypos][xpos] = "X";
                 ypos++;
             }
             if (left)
             {
-                a[ypos][xpos] = "X";
                 xpos--;
             }
         }
-        int count = 0;
         for (int i = 0; i < a.length; i++)
         {
             for (int j = 0; j < a[i].length; j++)
             {
                 if (a[i][j].equals("X"))
                 {
+                    visits.add(i + " " + j);
                     count++;
                 }
             }
@@ -138,9 +139,9 @@ public class Day6 {
         return count;
     }
 
-
     public static boolean isLoop(String[][] a, int x, int y)
     {
+        int len = a.length;
         int steps = 0;
         int ypos = x;
         int xpos = y;
@@ -148,25 +149,14 @@ public class Day6 {
         boolean right = false;
         boolean down = false;
         boolean left = false;
+        int checkForManyObs = 0;
         while (steps < 10000)
         {
-            if (xpos == 130)
+            if (xpos == len - 1 || ypos == len - 1 || xpos == 0 || ypos == 0)
             {
                 return false;
             }
-            if (ypos == 130)
-            {
-                return false;
-            }
-            if (xpos == -1)
-            {
-                return false;
-            }
-            if (ypos == -1)
-            {
-                return false;
-            }
-            try {
+            while (checkForManyObs < 2) {
                 if (up) {
                     if (a[ypos - 1][xpos].equals("#")) {
                         up = false;
@@ -191,44 +181,36 @@ public class Day6 {
                         up = true;
                     }
                 }
+                checkForManyObs++;
             }
-            catch (Exception _)
-            {
-
-            }
+            checkForManyObs = 0;
+            a[ypos][xpos] = "X";
             if (up)
             {
-                a[ypos][xpos] = "X";
                 ypos--;
-                steps++;
             }
             if (right)
             {
-                a[ypos][xpos] = "X";
                 xpos++;
-                steps++;
             }
             if (down)
             {
-                a[ypos][xpos] = "X";
                 ypos++;
-                steps++;
             }
             if (left)
             {
-                a[ypos][xpos] = "X";
                 xpos--;
-                steps++;
             }
+            steps++;
         }
         return true;
     }
+
 
     public static String[][] deepCopy(String[][] original) {
         if (original == null) {
             return null;
         }
-
         final String[][] result = new String[original.length][];
         for (int i = 0; i < original.length; i++) {
             result[i] = Arrays.copyOf(original[i], original[i].length);
@@ -237,16 +219,15 @@ public class Day6 {
         }
         return result;
     }
-
-
+    
     public static ArrayList<String> getFileData(String fileName) {
-        ArrayList<String> fileData = new ArrayList<String>();
+        ArrayList<String> fileData = new ArrayList<>();
         try {
             File f = new File(fileName);
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
                 String line = s.nextLine();
-                if (!line.equals(""))
+                if (!line.isEmpty())
                     fileData.add(line);
             }
             return fileData;
@@ -256,3 +237,4 @@ public class Day6 {
         }
     }
 }
+
