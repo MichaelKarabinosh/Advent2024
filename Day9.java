75825d60fb0cd771a84339099c1addce5399b62c
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-02-14 17:34:37 -0500

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day9 {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/InputFile");
+       String data = fileData.getFirst();
+
+       String disk = "";
+       int num = 0;
+       for (int i = 0; i < data.length(); i++)
+       {
+            String char1 = data.substring(i, i+1);
+            String numAsStr = num + "";
+           if (i % 2 ==  0)
+           {
+               disk +=  numAsStr.repeat(Integer.parseInt(char1));
+               num++;
+           }
+           else {
+               disk += ".".repeat(Integer.parseInt(char1));
+           }
+       }
+        long total = getTotal(disk);
+        System.out.println("part one: " + total);
+
+
+
+    }
+
+    private static long getTotal(String disk) {
+        ArrayList<Integer> freeSpaces = new ArrayList<>();
+        ArrayList<String> diskAsList = new ArrayList<>();
+        int numNums = 0;
+        for (int i = 0; i < disk.length(); i++)
+        {
+            diskAsList.add(disk.charAt(i) + "");
+            if (disk.charAt(i) == '.') {
+                freeSpaces.add(i);
+            }
+            else {
+                numNums++;
+            }
+        }
+        int index = diskAsList.size() - 1;
+        while (!freeSpaces.isEmpty())
+        {
+            while (diskAsList.get(index).equals("."))
+            {
+                index--;
+            }
+            diskAsList.set(freeSpaces.getFirst(), diskAsList.get(index));
+            index--;
+            freeSpaces.removeFirst();
+        }
+        long total = 0;
+        for (int i = 0; i < numNums; i++)
+        {
+            total += i * Long.parseLong(diskAsList.get(i));
+        }
+        return total;
+    }
+
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.isEmpty())
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
